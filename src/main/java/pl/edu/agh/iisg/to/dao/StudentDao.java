ce875ec1f19b6238ae522b0f2d189ae20901b6a0
Bartłomiej Szałach <onegrx@gmail.com>
2016-11-06 22:21:31 +0100

    Search students with StudentDao

--- src/main/java/pl/edu/agh/iisg/to/dao/StudentDao.java
+++ src/main/java/pl/edu/agh/iisg/to/dao/StudentDao.java
@@ -1,8 +1,10 @@
 package pl.edu.agh.iisg.to.dao;
 
+import org.hibernate.Session;
 import pl.edu.agh.iisg.to.model.Course;
 import pl.edu.agh.iisg.to.model.Student;
 
+import javax.persistence.PersistenceException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -11,23 +13,39 @@ import java.util.Optional;
 public class StudentDao extends GenericDao<Student> {
 
     public Optional<Student> create(final String firstName, final String lastName, final int indexNumber) {
-        //TODO [1] Implementacja stworzenia obiektu w bazie danych
-        return Optional.empty();
+        Student student = new Student(firstName, lastName, indexNumber);
+        try {
+            save(student);
+            return Optional.of(student);
+        } catch (PersistenceException e) {
+            return Optional.empty();
+        }
     }
 
     public Optional<Student> findById(final int id) {
-        //TODO [2] Implementacja znajdowania studenta poprzez id
-        return Optional.empty();
+        Session session = sessionFactory.openSession();
+        Student student = session.get(Student.class, id);
+        session.close();
+        return Optional.ofNullable(student);
     }
 
     public Optional<Student> findByIndexNumber(final int indexNumber) {
-        //TODO [3] Implementacja znajdowania studenta poprzez index
-        return Optional.empty();
+        Session session = sessionFactory.openSession();
+        Student student =
+                session.createQuery("select s from Student s where s.indexNumber = :index", Student.class)
+                .setParameter("index", indexNumber).getSingleResult();
+        session.close();
+        return Optional.ofNullable(student);
     }
 
     public List<Student> findAllByLastName(final String lastName) {
-        //TODO [4] Implementaca znajdowania studentów dla podanego nazwiska
-        return Collections.emptyList();
+        Session session = sessionFactory.openSession();
+        List<Student> students =
+                session.createQuery("select s from Student s where s.lastName = :lastName", Student.class)
+                .setParameter("lastName", lastName).getResultList();
+        session.close();
+        //Hibernate returns an empty list if no results are found instead of null
+        return students;
     }
 
     public Map<Course, Float> createReport(final Student student) {
